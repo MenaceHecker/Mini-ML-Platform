97eaf2cc55cfd4320a61d984a2c605703e6873de
MenaceHecker <thedynamic2508@gmail.com>
2026-01-22 15:00:22 -0500

    Changed scope of app declaration

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
 from app.api.ingest import router as ingest_router
 
-app.include_router(ingest_router)
-
 
 app = FastAPI(title="Mini ML Platform")
+app.include_router(ingest_router)
 
 @app.get("/health")
 def health():
