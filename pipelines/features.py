7172b431fe4b65a6f322db8db571cc74bb74adfb
MenaceHecker <thedynamic2508@gmail.com>
2026-01-22 12:08:47 -0500

    Added sample feature logic

--- pipelines/features.py
+++ pipelines/features.py
@@ -4,3 +4,9 @@ def build_features(input_path: str, output_path: str):
 
     #Basic cleaning here
     df = df.dropna()
+
+    # Example feature logic
+    df["RoomsPerHousehold"] = df["AveRooms"] / df["AveOccup"]
+    df["BedroomsPerRoom"] = df["AveBedrms"] / df["AveRooms"]
+
+    df.to_csv(output_path, index=False)
