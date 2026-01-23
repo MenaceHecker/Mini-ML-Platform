dfc1e40af79dcaf793dd7c37677ee48819fe3652
MenaceHecker <thedynamic2508@gmail.com>
2026-01-23 09:35:47 -0500

    Added dataframes

--- pipelines/training.py
+++ pipelines/training.py
@@ -5,3 +5,9 @@ from sklearn.ensemble import RandomForestRegressor
 
 def train_model(data_path: str):
     df = pd.read_csv(data_path)
+    X = df.drop("MedHouseVal", axis=1)
+    y = df["MedHouseVal"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
