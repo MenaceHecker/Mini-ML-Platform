6f209f0f1a13f374f0bc17609500a6dfc5873a7a
MenaceHecker <thedynamic2508@gmail.com>
2026-01-23 09:59:56 -0500

    Phase 3 testing completed

--- pipelines/training.py
+++ pipelines/training.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
@@ -16,8 +17,9 @@ def train_model(data_path: str):
     model.fit(X_train, y_train)
 
     preds = model.predict(X_test)
-    rmse = mean_squared_error(y_test, preds, squared=False)
     r2 = r2_score(y_test, preds)
+    mse = mean_squared_error(y_test, preds)
+    rmse = np.sqrt(mse)
 
     return {
         "rmse": rmse,
