8c610cae5796d2469f36c699b44a491a16c24ec4
MenaceHecker <thedynamic2508@gmail.com>
2026-01-23 09:40:22 -0500

    Training file completed, using rmse for errors

--- pipelines/training.py
+++ pipelines/training.py
@@ -11,3 +11,15 @@ def train_model(data_path: str):
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
+
+    model = RandomForestRegressor(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+
+    preds = model.predict(X_test)
+    rmse = mean_squared_error(y_test, preds, squared=False)
+    r2 = r2_score(y_test, preds)
+
+    return {
+        "rmse": rmse,
+        "r2": r2
+    }
