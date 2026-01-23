aaec32afc7899216eb5a4bd8e3c69682e3b8547b
MenaceHecker <thedynamic2508@gmail.com>
2026-01-23 15:08:16 -0500

    Tested MLFlow and Fast API

--- pipelines/training.py
+++ pipelines/training.py
@@ -1,11 +1,17 @@
-import numpy as np
 import pandas as pd
+import mlflow
+import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.ensemble import RandomForestRegressor
+import numpy as np
 
 def train_model(data_path: str):
+    mlflow.set_tracking_uri("file:./experiments")
+    mlflow.set_experiment("california-housing")
+
     df = pd.read_csv(data_path)
+
     X = df.drop("MedHouseVal", axis=1)
     y = df["MedHouseVal"]
 
@@ -13,13 +19,28 @@ def train_model(data_path: str):
         X, y, test_size=0.2, random_state=42
     )
 
-    model = RandomForestRegressor(n_estimators=100, random_state=42)
-    model.fit(X_train, y_train)
+    with mlflow.start_run():
+        model = RandomForestRegressor(
+            n_estimators=100,
+            random_state=42
+        )
 
-    preds = model.predict(X_test)
-    r2 = r2_score(y_test, preds)
-    mse = mean_squared_error(y_test, preds)
-    rmse = np.sqrt(mse)
+        model.fit(X_train, y_train)
+        preds = model.predict(X_test)
+
+        mse = mean_squared_error(y_test, preds)
+        rmse = np.sqrt(mse)
+        r2 = r2_score(y_test, preds)
+
+        # Log params
+        mlflow.log_param("n_estimators", 100)
+
+        # Log metrics
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+
+        # Log model artifact
+        mlflow.sklearn.log_model(model, "model")
 
     return {
         "rmse": rmse,
