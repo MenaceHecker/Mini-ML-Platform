c966398868b26be86feca7630ff85b683d2348ef
MenaceHecker <thedynamic2508@gmail.com>
2026-01-24 12:34:11 -0500

    inference setup complete

--- pipelines/inference.py
+++ pipelines/inference.py
@@ -1,2 +1,8 @@
 import mlflow.pyfunc
 
+MODEL_NAME = "CaliforniaHousingModel"
+
+def load_production_model():
+    model_uri = f"models:/{MODEL_NAME}/Production"
+    model = mlflow.pyfunc.load_model(model_uri)
+    return model
